29dd1f20bb044d3c3f6621f92c2434bc1f113d4f
Pirat <intake@list.ru>
2024-01-12 01:54:47 +0300

    Added create message command and command handler

---
+++ PiratMessages.Application/Messages/Commands/CreateMessage/CreateMessageCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace PiratMessages.Application.Messages.Commands.CreateMessage
+{
+    public class CreateMessageCommand : IRequest<Guid>
+    {
+        public Guid UserId { get; set; }
+        public Guid DestinationUserId { get; set; }
+        public string Details { get; set; }
+    }
+}
---
+++ PiratMessages.Application/Messages/Commands/CreateMessage/CreateMessageCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using PiratMessages.Application.Interfaces;
+using PiratMessages.Domain;
+
+namespace PiratMessages.Application.Messages.Commands.CreateMessage
+{
+    public class CreateMessageCommandHandler : IRequestHandler<CreateMessageCommand, Guid>
+    {
+        private readonly IMessagesDbContext _dbContext;
+
+        public CreateMessageCommandHandler(IMessagesDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Guid> Handle(CreateMessageCommand request,
+            CancellationToken cancellationToken)
+        {
+            var message = new Message
+            {
+                UserId = request.UserId,
+                DestinationUserId = request.DestinationUserId,
+                Details = request.Details,
+                Id = Guid.NewGuid(),
+                CreationDate = DateTime.Now,
+                EditDate = null
+            };
+
+            await _dbContext.Messages.AddAsync(message, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return message.Id;
+        }
+    }
+}
