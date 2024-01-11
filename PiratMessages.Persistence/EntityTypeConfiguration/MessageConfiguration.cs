da2ca9e58595a1aeee7ed17e42373b80198dec51
Pirat <intake@list.ru>
2024-01-11 09:00:24 +0300

    Added EF Package. Added entity type configuration.

---
+++ PiratMessages.Persistence/EntityTypeConfiguration/MessageConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PiratMessages.Domain;
+
+namespace PiratMessages.Persistence.EntityTypeConfiguration
+{
+    public class MessageConfiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.HasKey(message => message.Id);
+            builder.HasIndex(message => message.Id).IsUnique();
+        }
+    }
+}
