fe791add4f78fe156dedf5bf423e12b0bcbf3df1
Pirat <intake@list.ru>
2024-01-11 08:46:59 +0300

    Add project files.

---
+++ PiratMessages.Domain/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace PiratMessages.Domain
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid DestinationUserId { get; set; }
+        public string Details { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+    }
+}
