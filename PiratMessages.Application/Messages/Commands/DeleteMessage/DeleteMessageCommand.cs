92d3cf99912b1850543638ad36454860be4981b6
Pirat <intake@list.ru>
2024-01-13 19:07:48 +0300

    Added delete message command and command handler

---
+++ PiratMessages.Application/Messages/Commands/DeleteMessage/DeleteMessageCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace PiratMessages.Application.Messages.Commands.DeleteMessage
+{
+    public class DeleteMessageCommand : IRequest<Unit>
+    {
+        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
+    }
+}
---
+++ PiratMessages.Application/Messages/Commands/DeleteMessage/DeleteMessageCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using PiratMessages.Application.Common.Exceptions;
+using PiratMessages.Application.Interfaces;
+using PiratMessages.Domain;
+
+namespace PiratMessages.Application.Messages.Commands.DeleteMessage
+{
+    public class DeleteMessageCommandHandler : IRequestHandler<DeleteMessageCommand, Unit>
+    {
+        private readonly IMessagesDbContext _dbContext;
+        public DeleteMessageCommandHandler(IMessagesDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Unit> Handle(DeleteMessageCommand request,
+            CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Messages.FindAsync(new object[] { request.Id }, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Message), request.Id);
+            }
+
+            _dbContext.Messages.Remove(entity);
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
