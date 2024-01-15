cd10dd220cf3634235dbb07dd79daf37bd570272
Pirat <intake@list.ru>
2024-01-15 10:10:17 +0300

    Added Db initializing

--- PiratMessages.WebApi/Program.cs
+++ PiratMessages.WebApi/Program.cs
@@ -1,6 +1,21 @@
+using PiratMessages.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<MessagesDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception exception)
+    {
+       
+    }
+}
 
 app.Run();
