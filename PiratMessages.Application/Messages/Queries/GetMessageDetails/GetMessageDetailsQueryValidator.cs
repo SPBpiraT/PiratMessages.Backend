0cc24c0f45b33fcc30c6b571929e77b51358c5a3
Pirat <intake@list.ru>
2024-01-16 09:13:32 +0300

    Added Fluent Validation and validation behavior

---
+++ PiratMessages.Application/Messages/Queries/GetMessageDetails/GetMessageDetailsQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace PiratMessages.Application.Messages.Queries.GetMessageDetails
+{
+    public class GetMessageDetailsQueryValidator : AbstractValidator<GetMessageDetailsQuery>
+    {
+        public GetMessageDetailsQueryValidator()
+        {
+            RuleFor(message =>
+                message.Id).NotEqual(Guid.Empty);
+            RuleFor(message =>
+                message.UserId).NotEqual(Guid.Empty);
+        }
+    }
+}
---
+++ PiratMessages.Application/Messages/Queries/GetMessageList/GetMessageListQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace PiratMessages.Application.Messages.Queries.GetMessageList
+{
+    public class GetMessageListQueryValidator : AbstractValidator<GetMessageListQuery>
+    {
+        public GetMessageListQueryValidator()
+        {
+            RuleFor(x =>
+                x.UserId).NotEqual(Guid.Empty);
+        }
+    }
+}
