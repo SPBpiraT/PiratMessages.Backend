cdc82cf60418b65520d8f1b341bb141711d00c4f
Pirat <intake@list.ru>
2024-01-27 13:19:50 +0300

    Added api and Db dockerization using docker compose

--- PiratMessages.Persistence/DependencyInjection.cs
+++ PiratMessages.Persistence/DependencyInjection.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PiratMessages.Application.Interfaces;
 
@@ -7,16 +6,22 @@ namespace PiratMessages.Persistence
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddPersistence(this IServiceCollection services,
-            IConfiguration configuration)
+        public static IServiceCollection AddPersistence(this IServiceCollection services)
         {
-            var connectionString = configuration["DbConnection"];
+            var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+            var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+            var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+
+            var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword}";
+
             services.AddDbContext<MessagesDbContext>(options =>
             {
                 options.UseSqlServer(connectionString, b => b.MigrationsAssembly("Migrations"));
             });
+
             services.AddScoped<IMessagesDbContext>(provider =>
                 provider.GetService<MessagesDbContext>());
+
             return services;
         }
     }
